28cc39a6bb04a792bc965eef77a307d017fd3573
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2019-07-09 21:27:37 +0500

    Changed login to same theme

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity } from 'react-native'
 import firebase from 'react-native-firebase';
 
 export default class Login extends React.Component {
@@ -32,9 +32,9 @@ export default class Login extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Login</Text>
+                <Text style={{ fontSize: 20 }}>Login</Text>
                 {this.state.errorMessage &&
-                    <Text style={{ color: 'red' }}>
+                    <Text style={{ color: 'red', fontSize: 20 }}>
                         {this.state.errorMessage}
                     </Text>}
                 <TextInput
@@ -52,11 +52,22 @@ export default class Login extends React.Component {
                     onChangeText={password => this.setState({ password })}
                     value={this.state.password}
                 />
-                <Button title="Login" onPress={this.handleLogin} />
-                <Button
-                    title="Don't have an account? Sign Up"
+                <TouchableOpacity
+                    style={styles.SubmitButtonStyle}
+                    activeOpacity={.5}
+                    onPress={this.handleLogin}
+                >
+
+                    <Text style={styles.TextStyle}> Login </Text>
+
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.SubmitButtonStyle}
+                    activeOpacity={.5}
                     onPress={() => this.props.navigation.navigate('SignUp')}
-                />
+                >
+                    <Text style={styles.TextStyle}> Register </Text>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -66,13 +77,34 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: '#FFA500'
     },
     textInput: {
         height: 40,
         width: '90%',
         borderColor: 'gray',
         borderWidth: 1,
-        marginTop: 8
+        marginTop: 8,
+        backgroundColor: 'white'
+    },
+    SubmitButtonStyle: {
+
+        marginTop: 10,
+        paddingTop: 15,
+        paddingBottom: 15,
+        marginLeft: 30,
+        marginRight: 30,
+        backgroundColor: 'black',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'black',
+        width: 150,
+        borderRadius: 60
+    },
+
+    TextStyle: {
+        color: '#fff',
+        textAlign: 'center',
     }
 })
